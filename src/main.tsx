7e74017def00caf3fb11d8e7c0008a5891c91ba0
dolodorsey <dolodorsey@gmail.com>
2026-08-09 07:50:19 -0400

    Add real moderated business reviews to BLACK PAGES
    * fix: expose moderated Black Pages reviews
    
    * fix: mount Black Pages review interaction host

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import LocationInteractionHost from './LocationInteractionHost'
+import ReviewInteractionHost from './ReviewInteractionHost'
 import './styles.css'
 import './current-media.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LocationInteractionHost/>
+    <ReviewInteractionHost/>
     <App/>
   </StrictMode>,
 )

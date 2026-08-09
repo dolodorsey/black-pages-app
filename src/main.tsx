940c490d904ebc50e1cf4337e1d31ca6892234de
dolodorsey <dolodorsey@gmail.com>
2026-08-09 07:15:22 -0400

    fix: mount Black Pages location interaction host

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import LocationInteractionHost from './LocationInteractionHost'
 import './styles.css'
 import './current-media.css'
-createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>)
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <LocationInteractionHost/>
+    <App/>
+  </StrictMode>,
+)
